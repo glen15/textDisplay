702414c74723df0c190f7d5383d73bbf1239e035
이정훈 <ijeonghun@jeonghunlee.local>
2023-10-20 16:53:24 +0900

    gitactions

--- nvm&mysql.sh
+++ nvm&mysql.sh
@@ -8,7 +8,6 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 nvm install --lts
-# sleep 5
 sudo apt-get install npm -y
 sudo apt install mysql-client-core-8.0
 node -v
