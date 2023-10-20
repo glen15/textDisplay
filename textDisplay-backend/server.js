2c0fd54eeda27cc479a0860b8b8e4d4f353c41b3
이정훈 <ijeonghun@jeonghunlee.local>
2023-10-20 17:30:02 +0900

    ;

--- textDisplay-backend/server.js
+++ textDisplay-backend/server.js
@@ -33,7 +33,7 @@ app.get('/api/text', (req, res) => {
 app.post('/api/text', (req, res) => {
     const { text, username } = req.body;
     console.log(req.body)
-    const exclamationText = `${text} ...아마도...`;
+    const exclamationText = `${text} ...아마도...!`;
     connection.query('INSERT INTO texts SET ?', { text: exclamationText, username }, (error) => {
         if (error) throw error;
         res.sendStatus(201);
